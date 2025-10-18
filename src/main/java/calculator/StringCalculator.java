4c0d833b0cad88bc865cc8bdb0b028c21e1c7c1c
seoboyoung18 <135713836+seoboyoung18@users.noreply.github.com>
2025-10-19 02:43:39 +0900

    예외처리

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,45 @@
+package calculator;
+
+public class StringCalculator {
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = input.split("[,|:]");
+
+        if (numbers.length == 1) {
+            String value = numbers[0];
+            if (!value.matches("\\d+")) {
+                throw new IllegalArgumentException("잘못된 입력입니다: " + value);
+            }
+
+            throw new IllegalArgumentException("구분자가 포함된 형식이 아닙니다: " + value);
+        }
+
+        int sum = 0;
+        for (String num : numbers) {
+            if (!num.matches("\\d+")) {
+                throw new IllegalArgumentException("잘못된 입력: " + num);
+            }
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
+    }
+
+    private static String[] splitInput(String input) {
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+
+            if (delimiterIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 형식이 잘못되었습니다.");
+            }
+            String delimiter = input.substring(2, delimiterIndex);
+            String numbers = input.substring(delimiterIndex + 1);
+            return numbers.split(delimiter);
+        }
+        return input.split("[,:]");
+
+    }
+}

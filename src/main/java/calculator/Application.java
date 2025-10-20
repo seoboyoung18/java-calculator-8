65f34b5d7e7042bc5a8351547a5a97e0425feec0
seoboyoung18 <135713836+seoboyoung18@users.noreply.github.com>
2025-10-20 19:29:59 +0900

    try-catch 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,11 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
+        try {
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+            String input = Console.readLine();
 
-        //결과 출력
-        int result = StringCalculator.add(input);
-        System.out.println("결과 : " + result);
+            int result = StringCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
